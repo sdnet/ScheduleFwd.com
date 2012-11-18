071138f6d0772494caf1d44bc49d073b9d2a2647
Thomas Smallwood <tomwildcat@Thomass-MacBook-Air.local>
2012-11-18 16:13:37 -0700

    new app created….

---
+++ mobile/ios/ScheduleForward/ScheduleForward/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ScheduleForward
+//
+//  Created by Thomas Smallwood on 11/18/12.
+//  Copyright (c) 2012 Thomas Smallwood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ mobile/ios/ScheduleForward/ScheduleForward/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ScheduleForward
+//
+//  Created by Thomas Smallwood on 11/18/12.
+//  Copyright (c) 2012 Thomas Smallwood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
