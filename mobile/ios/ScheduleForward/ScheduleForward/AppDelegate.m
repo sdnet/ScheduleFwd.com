82d4f2d0afaf33db411b44967d9bf72e748249dc
Thomas Smallwood <tomwildcat@Thomass-MacBook-Air.local>
2012-11-18 21:13:17 -0700

    added TapKu Framework to app

--- mobile/ios/ScheduleForward/ScheduleForward/AppDelegate.m
+++ mobile/ios/ScheduleForward/ScheduleForward/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import "MyCalendarViewController_iPhone.h"
+
 
 @implementation AppDelegate
 
@@ -15,6 +17,10 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    
+    
+    
+    
     [self.window makeKeyAndVisible];
     return YES;
 }
---
+++ mobile/ios/ScheduleForward/ScheduleForward/LoginViewController_iPhone.m
@@ -0,0 +1,38 @@
+//
+//  LoginViewController_iPhone.m
+//  ScheduleForward
+//
+//  Created by Thomas Smallwood on 11/18/12.
+//  Copyright (c) 2012 Thomas Smallwood. All rights reserved.
+//
+
+#import "LoginViewController_iPhone.h"
+
+@interface LoginViewController_iPhone ()
+
+@end
+
+@implementation LoginViewController_iPhone
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
